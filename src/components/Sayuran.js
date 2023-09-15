485cb4096c9f7af76438b3036cbbfbd1088697c3
fifovalle <fifanaufal10@gmail.com>
2023-09-15 08:46:18 +0700

    Menambahkan tombol kategori agar bisa di klik

--- src/components/Sayuran.js
+++ src/components/Sayuran.js
@@ -1,11 +1,21 @@
 // FILE REACT
-import React from "react";
+import React, { useState } from "react";
 import { ArrowSmRightIcon } from "@heroicons/react/outline";
 // FILE SAYA
 import { DaftarSayuran } from "../utils/Data";
 import { DaftarTombol } from "../utils/Data";
 
 const Sayuran = () => {
+  const [tombolAktif, aturTombolAktif] = useState(null);
+  const [sayuran, aturSayuran] = useState(DaftarSayuran);
+  const saring = (kategori) => {
+    const saringSayuran =
+      kategori === "Semuanya"
+        ? DaftarSayuran
+        : DaftarSayuran.filter((sayuran) => sayuran.kategori === kategori);
+    aturSayuran(saringSayuran);
+    aturTombolAktif(kategori);
+  };
   return (
     <div className="max-w-[1520px] mx-auto px-4 py-12">
       <h1 className="text-green-500 font-bold text-2xl text-center py-2">
@@ -14,15 +24,20 @@ const Sayuran = () => {
       <div className="lg:flex md:grid md:grid-cols-4 sm:grid sm:grid-cols-3 custom:grid custom:grid-cols-2 justify-center md:justify-center">
         {DaftarTombol.map((tombol) => (
           <button
+            onClick={() => saring(tombol.nama)}
             key={tombol.id}
-            className="m-1 border-green-600 text-white bg-green-600 hover:bg-white hover:text-green-600 hover:border-green-600"
+            className={`m-1 border-green-600 text-white ${
+              tombol.nama === tombolAktif
+                ? "bg-white text-green-600 border-green-600"
+                : "bg-green-600 hover:bg-white hover:text-green-600 hover:border-green-600"
+            }`}
           >
             {tombol.nama}
           </button>
         ))}
       </div>
       <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4">
-        {DaftarSayuran.map((sayuran) => (
+        {sayuran.map((sayuran) => (
           <div
             key={sayuran.id}
             className="border-none hover:scale-105 duration-300"
